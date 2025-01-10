d78245c511172b681cb833ac9bf3efa5ccb78f25
Ritesh Kumar Khandelwal <ritekumar@adobe.com>
2025-01-10 22:37:18 +0530

    Issue fixes

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,8 +1,6 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
-export default function decorate(block) {
-  console.log(block);
-  
+export default function decorate(block) {  
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
@@ -17,6 +15,5 @@ export default function decorate(block) {
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
-  console.log(block);
   
 }
